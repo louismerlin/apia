d142245c4f33380abdd42ed29954b42f0f0bc688
Louis Merlin <louis.merlin@epfl.ch>
2018-10-26 14:56:53 +0200

    Write first version, a simple hit counter

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { Router } = require('express')
+const apia = Router()
+
+const hits = {}
+
+apia.use((req, res, next) => {
+  hits[req.url] = hits[req.url] + 1 || 1
+  next()
+})
+
+// API request with relevant info
+apia.get('/apia', (req, res) => 
+  res.send(hits)
+)
+
+// Graphical view of relevant info
+apia.get('/grapia', (req, res) =>
+  res.send(hits)
+)
+
+module.exports = apia
---
+++ test/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const apia = require('../src/index')
+
+const app = express()
+const port = 3000
+
+app.use(apia)
+
+app.get('/', (req, res) =>
+  res.send('Hello, World !')
+)
+
+app.get('/:name', (req, res) =>
+  res.send(`Hello, ${req.params.name} !`)
+)
+
+app.listen(port, () => console.log(`Test app listening on port ${port}`))
